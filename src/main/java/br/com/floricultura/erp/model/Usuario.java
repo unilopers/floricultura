a04ae669d6fc83e06d74c0e9e12b5a93809fbb9e
Antonio Marcos <antoniobrmarcos@gmail.com>
2025-11-22 14:16:31 -0300

    :passport_control: feat: Criação da entidade Usuario para autenticação

---
+++ src/main/java/br/com/floricultura/erp/model/Usuario.java
@@ -0,0 +1,24 @@
+package br.com.floricultura.erp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "USUARIO")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
---
+++ src/main/java/br/com/floricultura/erp/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.floricultura.erp.repository;
+
+import br.com.floricultura.erp.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
