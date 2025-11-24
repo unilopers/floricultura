ba10cb79b5f1292b9aaf33709fd314f94871f3f3
Joaorm7 <joaogabrielseti311006@gmail.com>
2025-11-24 19:30:52 -0300

    :recycle: refactor: Lógica de Vendas adaptada para API REST

--- src/main/java/br/com/floricultura/erp/controller/PedidoController.java
+++ src/main/java/br/com/floricultura/erp/controller/PedidoController.java
@@ -1,35 +1,22 @@
-package br.com.floricultura.erp.controller;
+ package br.com.floricultura.erp.controller;
 
-import br.com.floricultura.erp.model.*;
-import br.com.floricultura.erp.repository.ClienteRepository;
-import br.com.floricultura.erp.repository.ProdutoRepository;
+import br.com.floricultura.erp.model.Pedido;
 import br.com.floricultura.erp.services.PedidoService;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/pedidos")
 @RequiredArgsConstructor
-@RequestMapping("/pedido")
 public class PedidoController {
 
-    private final ProdutoRepository produtoRepository;
-    private final ClienteRepository clienteRepository;
     private final PedidoService pedidoService;
 
-    @GetMapping("/novo")
-    public String novoPedido(Model model) {
-        model.addAttribute("pedido", new Pedido());
-        model.addAttribute("produtos", produtoRepository.findAll());
-        model.addAttribute("clientes", clienteRepository.findAll());
-        return "pedido-form";
-    }
+    @PostMapping
+    public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
 
-    @PostMapping("/salvar")
-    public String salvarPedido(@ModelAttribute Pedido pedido) {
-        pedidoService.criarPedido(pedido);
-        return "redirect:/pedido/novo?sucesso";
+        Pedido novoPedido = pedidoService.criarPedido(pedido);
+        return ResponseEntity.ok(novoPedido);
     }
 }
